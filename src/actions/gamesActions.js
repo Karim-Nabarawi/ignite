0a8fb65d907ae6492fc4062252aa3c14021fd7e6
karim-nabarawi <k.nabarawi@gmail.com>
2021-01-04 15:16:43 +0200

    Setting up rest of API

--- src/actions/gamesActions.js
+++ src/actions/gamesActions.js
@@ -1,14 +1,19 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { newGamesURL, popularGamesURL, upcomingGamesURL } from "../api";
 
 //Action creator
 
 export const loadGames = () => async (dispatch) => {
   const popularData = await axios.get(popularGamesURL());
+  const upcomingData = await axios.get(upcomingGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
+  console.log(newGamesURL());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
